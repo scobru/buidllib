0eb8d722eaa170adc7e7d6b116bbad0dbc73d3cd
scobru <scobru1988@gmail.com>
2023-07-11 00:13:59 +0200

    add modifier. change function name

--- contracts/utils/TokenChecker.sol
+++ contracts/utils/TokenChecker.sol
@@ -7,38 +7,139 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 contract TokenChecker {
-    function userHasERC20(
+    // MODIFIERS
+
+    modifier requireERC20Balance(
         address tokenAddress,
-        address userAddress,
+        address accountAddress,
+        uint256 minBalance
+    ) {
+        require(
+            _hasERC20Balance(tokenAddress, accountAddress, minBalance),
+            "TokenChecker: Insufficient ERC20 token balance"
+        );
+        _;
+    }
+
+    modifier requireERC721Ownership(
+        address tokenAddress,
+        uint256 tokenId,
+        address accountAddress
+    ) {
+        require(
+            _hasERC721Ownership(tokenAddress, tokenId, accountAddress),
+            "TokenChecker: Account does not own the ERC721 token"
+        );
+        _;
+    }
+
+    modifier requireERC721Balance(
+        address tokenAddress,
+        address accountAddress,
+        uint256 minAmount
+    ) {
+        require(
+            _hasERC721Balance(tokenAddress, accountAddress, minAmount),
+            "TokenChecker: Insufficient ERC721 token quantity"
+        );
+        _;
+    }
+
+    modifier requireERC1155Balance(
+        address tokenAddress,
+        uint256 tokenId,
+        address accountAddress,
+        uint256 minBalance
+    ) {
+        require(
+            _hasERC1155Balance(
+                tokenAddress,
+                tokenId,
+                accountAddress,
+                minBalance
+            ),
+            "TokenChecker: Insufficient ERC1155 token balance"
+        );
+        _;
+    }
+
+    // PUBLIC FUNCTIONS
+
+    function checkERC20Balance(
+        address tokenAddress,
+        address accountAddress,
         uint256 minBalance
     ) public view returns (bool) {
-        return IERC20(tokenAddress).balanceOf(userAddress) >= minBalance;
+        return _hasERC20Balance(tokenAddress, accountAddress, minBalance);
     }
 
-    function userOwnsERC721(
+    function checkERC721Ownership(
         address tokenAddress,
         uint256 tokenId,
-        address userAddress
+        address accountAddress
     ) public view returns (bool) {
-        return IERC721(tokenAddress).ownerOf(tokenId) == userAddress;
+        return _hasERC721Ownership(tokenAddress, tokenId, accountAddress);
     }
 
-    function userHasERC721(
+    function checkERC721Balance(
         address tokenAddress,
-        address userAddress,
+        address accountAddress,
         uint256 minAmount
     ) public view returns (bool) {
-        return IERC721(tokenAddress).balanceOf(userAddress) >= minAmount;
+        return _hasERC721Balance(tokenAddress, accountAddress, minAmount);
     }
 
-    function userHasERC1155(
+    function checkERC1155Balance(
         address tokenAddress,
         uint256 tokenId,
-        address userAddress,
+        address accountAddress,
         uint256 minBalance
     ) public view returns (bool) {
         return
-            IERC1155(tokenAddress).balanceOf(userAddress, tokenId) >=
+            _hasERC1155Balance(
+                tokenAddress,
+                tokenId,
+                accountAddress,
+                minBalance
+            );
+    }
+
+    // INTERNAL FUNCTIONS
+
+    function _hasERC20Balance(
+        address tokenAddress,
+        address accountAddress,
+        uint256 minBalance
+    ) internal view returns (bool) {
+        return IERC20(tokenAddress).balanceOf(accountAddress) >= minBalance;
+    }
+
+    function _hasERC721Ownership(
+        address tokenAddress,
+        uint256 tokenId,
+        address accountAddress
+    ) internal view returns (bool) {
+        IERC721 tokenContract = IERC721(tokenAddress);
+        return tokenContract.ownerOf(tokenId) == accountAddress;
+    }
+
+    function _hasERC721Balance(
+        address tokenAddress,
+        address accountAddress,
+        uint256 minAmount
+    ) internal view returns (bool) {
+        IERC721 tokenContract = IERC721(tokenAddress);
+        return tokenContract.balanceOf(accountAddress) >= minAmount;
+    }
+
+    function _hasERC1155Balance(
+        address tokenAddress,
+        uint256 tokenId,
+        address accountAddress,
+        uint256 minBalance
+    ) internal view returns (bool) {
+        return
+            IERC1155(tokenAddress).balanceOf(accountAddress, tokenId) >=
             minBalance;
     }
 }
--- test/TokenChecker.t.ts
+++ test/TokenChecker.t.ts
@@ -2,28 +2,20 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('TokenChecker', function () {
-  let accounts: { address: any }[],
-    mockERC1155: {
-      mint: (arg0: any, arg1: number, arg2: number, arg3: string) => any;
-      address: any;
-    },
-    mockERC20: { address: any },
-    mockERC721: { mint: (arg0: any, arg1: number) => any; address: any },
-    tokenChecker: {
-      userHasERC20: (arg0: any, arg1: any, arg2: any) => any;
-      userOwnsERC721: (arg0: any, arg1: number, arg2: any) => any;
-      userHasERC721: (arg0: any, arg1: any, arg2: number) => any;
-      userHasERC1155: (arg0: any, arg1: number, arg2: any, arg3: number) => any;
-    };
+  let accounts: { address: any; }[],
+    tokenChecker: { checkERC20Balance: (arg0: any, arg1: any, arg2: any) => any; checkERC721Ownership: (arg0: any, arg1: number, arg2: any) => any; checkERC721Balance: (arg0: any, arg1: any, arg2: number) => any; checkERC1155Balance: (arg0: any, arg1: number, arg2: any, arg3: number) => any; },
+    mockERC20: { address: any; },
+    mockERC721: { mint: (arg0: any, arg1: number) => any; address: any; },
+    mockERC1155: { mint: (arg0: any, arg1: number, arg2: number, arg3: string) => any; address: any; };
 
   before(async () => {
     accounts = await ethers.getSigners();
 
+    const TokenChecker = await ethers.getContractFactory('TokenChecker');
+    tokenChecker = await TokenChecker.deploy();
+
     const MockERC20 = await ethers.getContractFactory('MockERC20');
-    mockERC20 = await MockERC20.deploy(
-      accounts[0].address,
-      ethers.utils.parseEther('1000')
-    );
+    mockERC20 = await MockERC20.deploy(accounts[0].address, ethers.utils.parseEther('1000'));
 
     const MockERC721 = await ethers.getContractFactory('MockERC721');
     mockERC721 = await MockERC721.deploy();
@@ -32,13 +24,10 @@ describe('TokenChecker', function () {
     const MockERC1155 = await ethers.getContractFactory('MockERC1155');
     mockERC1155 = await MockERC1155.deploy();
     await mockERC1155.mint(accounts[0].address, 1, 1000, '0x');
-
-    const TokenChecker = await ethers.getContractFactory('TokenChecker');
-    tokenChecker = await TokenChecker.deploy();
   });
 
   it('Should return true if the user has enough ERC20 tokens', async function () {
-    const result = await tokenChecker.userHasERC20(
+    const result = await tokenChecker.checkERC20Balance(
       mockERC20.address,
       accounts[0].address,
       ethers.utils.parseEther('1')
@@ -47,7 +36,7 @@ describe('TokenChecker', function () {
   });
 
   it('Should return false if the user does not have enough ERC20 tokens', async function () {
-    const result = await tokenChecker.userHasERC20(
+    const result = await tokenChecker.checkERC20Balance(
       mockERC20.address,
       accounts[0].address,
       ethers.utils.parseEther('2000')
@@ -55,8 +44,9 @@ describe('TokenChecker', function () {
     expect(result).to.equal(false);
   });
 
+
   it('Should return true if the user owns the ERC721 token', async function () {
-    const result = await tokenChecker.userOwnsERC721(
+    const result = await tokenChecker.checkERC721Ownership(
       mockERC721.address,
       1,
       accounts[0].address
@@ -65,7 +55,7 @@ describe('TokenChecker', function () {
   });
 
   it('Should return true if the user has enough ERC721 tokens', async function () {
-    const result = await tokenChecker.userHasERC721(
+    const result = await tokenChecker.checkERC721Balance(
       mockERC721.address,
       accounts[0].address,
       1
@@ -74,17 +64,17 @@ describe('TokenChecker', function () {
   });
 
   it('Should return true if the user has enough ERC1155 tokens', async function () {
-    const result = await tokenChecker.userHasERC1155(
+    const result = await tokenChecker.checkERC1155Balance(
       mockERC1155.address,
       1,
       accounts[0].address,
-      1
+      1000
     );
     expect(result).to.equal(true);
   });
 
   it('Should return false if the user does not have enough ERC1155 tokens', async function () {
-    const result = await tokenChecker.userHasERC1155(
+    const result = await tokenChecker.checkERC1155Balance(
       mockERC1155.address,
       1,
       accounts[0].address,
